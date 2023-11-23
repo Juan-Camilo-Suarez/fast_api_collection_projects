6654f56708c458501f92ac1e06e8ba6adf38f6c4
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2023-11-23 12:56:09 +0300

    + validation in query parameters whit Query

--- app_validation/main.py
+++ app_validation/main.py
@@ -1,8 +1,14 @@
 from datetime import date
 
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, Query
 
 app = FastAPI()
+
+
+@app.get('/')
+def get_root():
+    return {'message': 'Hello World'}
 
 
 @app.get('/hotels')
@@ -11,10 +17,11 @@ def get_hotels(
         date_from: date,
         date_to: date,
         # optional validation values
-        has_spa: int = None,
-        starts: bool = None
+        has_spa: bool = None,
+        starts: int = Query(default=None, gt=1, le=5)
 ):
-    return date_from, date_to
+    return date_from, date_to, has_spa, starts
 
-# if __name__ == '__main__':
-#     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="127.0.0.1", port=8000)
